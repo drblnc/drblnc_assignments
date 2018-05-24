3d957e5e249853a6b4a7c82bba2e20d4369bf446
Debbie Blanchard <debbie.blanchard1@gmail.com>
2018-05-24 13:07:27 -0400

    assignment 1

---
+++ p5 sketch/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+createCanvas(windowWidth, windowHeight).position(0, 0);
+}
+
+function draw() {
+	// p5 Color object
+background(color(0, 0, 255));
+noStroke();
+fill("#f49e42")
+ ellipse(width/2, height/2, 80, 80);
+
+}
